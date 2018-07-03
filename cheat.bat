eb53f7add5fbb759057e36bf5cb61b81867eab97
caxdcm <yunaisdaddy@gmail.com>
2018-07-03 18:52:01 +0700

    lennn

---
+++ cheat.bat
@@ -0,0 +1,26 @@
+@echo off
+cd %~dp0
+setlocal enabledelayedexpansion
+
+if not exist php\php.exe (
+	echo PHP wasn't detected; we'll download and install it for you.
+	PowerShell -ExecutionPolicy Unrestricted -File "downloadphp.ps1"
+)
+
+if not exist php\php.exe (
+	echo Failed to setup php, try doing it manually
+	pause
+	exit
+)
+
+if not exist token.txt (
+	echo(
+	echo Go to https://steamcommunity.com/saliengame/gettoken and save that page as token.txt file
+	echo(
+	pause
+)
+
+echo The script can be terminated at any time by pressing Ctrl-C
+
+php\php.exe -f cheat.php
+pause
